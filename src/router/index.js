b7421ba34e5a9607581c31f230477d16d029d806
xuannn1 <zyx7153841@126.com>
2019-12-02 12:47:30 +0800

    构建项目、路由、element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/LoginIndex.vue")
   },
   {
     path: "/about",
@@ -22,7 +25,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history"
 });
 
 export default router;
---
+++ src/views/LoginIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    这里登陆页
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  background-color: #344a5f;
+  height: 100vh;
+}
+</style>
